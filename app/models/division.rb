c1a2700cb22c887e27b2db47d14389012fcccfd4
Student <example@example.com>
2020-01-15 16:17:47 -0800

    finish many to many logic...

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,4 +1,6 @@
 class Division < ApplicationRecord
+  scope :war, -> { where(name: "War") }
+  scope :name, -> (name_parameter) { where(name: name_parameter) }
     has_and_belongs_to_many(:projects)
   has_many :employees, dependent: :destroy
   validates :name, presence: true
